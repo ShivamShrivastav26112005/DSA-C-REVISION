cecab30352ed4967ab9ddade5ecfb93257e1554b
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-05-09 10:53:46 +0530

    First Program in C++.

---
+++ Introduction To C++/firstprogram.cpp
@@ -0,0 +1,17 @@
+// Installation of VS code and code editor 
+
+// Run our first C++ program 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<"Hello Library";
+// }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello World";
+    return 0;
+}
