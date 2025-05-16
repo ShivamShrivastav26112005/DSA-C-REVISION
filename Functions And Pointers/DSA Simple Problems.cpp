ca0aca18465f699076b6a640b315fa5ab7e26698
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-05-16 19:39:01 +0530

    Added more questions.

--- Functions And Pointers/DSA Simple Problems.cpp
+++ Functions And Pointers/DSA Simple Problems.cpp
@@ -19,3 +19,66 @@ int main(){
     gcd(a,b);
 }
 // 12
+
+
+
+
+// Q-> find the maximum of three given no.s by using fun.
+// Method-1 -: by printing the fun. 
+
+#include<iostream>
+using namespace std;
+
+int maxi(int a, int b, int c){
+    if(a>b && a>c){
+        return a;
+    }else if(b>a && b>c){
+        return b;
+    }else{
+        return c;
+    }
+}
+
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<maxi(a,b,c);
+}
+
+
+// 25
+// 36
+// 63
+// 63
+
+
+
+
+
+
+// Method-2 -: by calling the fun. 
+#include<iostream>
+using namespace std;
+
+int maxi(int a, int b, int c){
+    if(a>b && a>c){
+        cout<<a;
+    }else if(b>a && b>c){
+        cout<<b;
+    }else{
+        cout<<c;
+    }
+}
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    maxi(a,b,c);
+}
+
+// 56
+// 32
+// 28
+// 56
+
+
+
