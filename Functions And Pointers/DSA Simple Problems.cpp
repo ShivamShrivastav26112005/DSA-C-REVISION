d5fdf7078a13f3922297b7b97a6b8131ad5431c6
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-05-17 18:36:30 +0530

    Added New Problems.

--- Functions And Pointers/DSA Simple Problems.cpp
+++ Functions And Pointers/DSA Simple Problems.cpp
@@ -82,3 +82,74 @@ int main(){
 
 
 
+
+
+// Method-3 -: by using extra variable. 
+
+#include<iostream>
+using namespace std;
+
+int maxi(int a, int b, int c){
+    int mx=1;
+    if(a>b && a>c){
+        mx=a;
+    }else if(b>a && b>c){
+        mx=b;
+    }else{
+        mx=c;
+    }
+    cout<<mx;
+}
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    maxi(a,b,c);
+}
+
+// 25
+// 63
+// 32
+// 63
+
+
+
+
+
+
+
+// Q-> Print the factorials of first n numbers. 
+// Method-1 -: 
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    int fact=1;
+    for(int i=2; i<=n; i++){
+        fact*=i;
+    }
+    cout<<fact<<endl;
+}
+
+int main(){
+    // only used for taking a i/p -: 
+    int n;
+    cout<<"Enter n : "<<endl;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        fact(i);
+    }
+
+}
+
+
+// Enter n : 
+// 5
+// 1
+// 2
+// 6
+// 24
+// 120
+
+
+
+
