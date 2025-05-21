6c91322b922dda0863fa5c0ee4dd67a932981c2d
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-05-21 19:18:01 +0530

    Pointers In C++

---
+++ Pointers/Pointers.cpp
@@ -0,0 +1,60 @@
+
+// Q-> Swap two no.s. with using function. 
+// with and without using third vari. 
+
+// #include<iostream>
+// using namespace std;
+// void swapping(int a, int b){
+//     // int temp=a;
+//     // a=b;
+//     // b=temp;
+
+//     // OR -: 
+
+//     a=a+b;
+//     b=a-b;
+//     a=a-b; 
+// }
+// int main(){
+//     int a=4;
+//     int b=6;
+//     cout<<a<<" "<<b<<endl;
+
+//     swapping(a,b);
+    
+//     cout<<a<<" "<<b<<endl;
+
+// }
+
+//          ****** Not swapped -: ****** 
+// ***** becoz this is pass by value not reference(only pass values not address) *****
+// ******** if you want, then send their addresses using (Pointers or References) ********
+// 4 6
+// 4 6
+
+
+
+//                  ******* Pointers -: *********
+
+
+// // sizeof() fun. -: to find the size of the data type -: 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     cout<<sizeof(int)<<endl;
+//     cout<<sizeof(float)<<endl;
+//     cout<<sizeof(char)<<endl;
+//     cout<<sizeof(double)<<endl;
+//     cout<<sizeof(bool)<<endl;
+//     cout<<sizeof(long)<<endl;
+//     cout<<sizeof(long long)<<endl;
+
+// }
+
+// // 4
+// // 4
+// // 1
+// // 8
+// // 1
+// // 4
+// // 8
