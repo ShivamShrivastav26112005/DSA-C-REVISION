660cbd4c77174c805e2e0d06fb05b2c64e55fb46
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-07-18 22:58:03 +0530

    Added  More five arrays question.

--- Final Revision Arrays Code.cpp
+++ Final Revision Arrays Code.cpp
@@ -46,3 +46,54 @@ void reverse(int arr[], int n) {
         end--;
     }
 }
+
+
+6. Check if Sorted. 
+bool isSorted(int arr[], int n) {
+    for(int i = 1; i < n; i++)
+        if(arr[i] < arr[i-1]) return false;
+    return true;
+}
+
+
+
+7. Remove Duplicates from Sorted Array. 
+int removeDuplicates(int arr[], int n) {
+    int res = 1;
+    for(int i = 1; i < n; i++)
+        if(arr[i] != arr[res-1])
+            arr[res++] = arr[i];
+    return res;
+}
+
+
+
+8. Left Rotate by 1
+void leftRotate(int arr[], int n) {
+    int temp = arr[0];
+    for(int i = 1; i < n; i++)
+        arr[i-1] = arr[i];
+    arr[n-1] = temp;
+}
+
+
+
+9. Move Zeros to End
+void moveZeros(int arr[], int n) {
+    int count = 0;
+    for(int i = 0; i < n; i++)
+        if(arr[i] != 0) swap(arr[i], arr[count++]);
+}
+
+
+
+10. Kadane's Algorithm (Max Subarray Sum)
+int maxSubarraySum(int arr[], int n) {
+    int maxSum = arr[0], currSum = arr[0];
+    for(int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
+}
+
