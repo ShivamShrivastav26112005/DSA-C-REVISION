5007ba5e64fe1eb77af0e4fb65f9b128a79a88a8
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-08-02 19:38:55 +0530

    Added More Problems

--- Stack DS Revision/Part1_Basic_Stacks.cpp
+++ Stack DS Revision/Part1_Basic_Stacks.cpp
@@ -66,8 +66,6 @@ int main(){
 
 
 
-
-
 // Problem -: Push element at the bottom of the stack -:
 
 #include<iostream>
@@ -80,9 +78,8 @@ int main(){
     st.push(30);
     st.push(40);
     st.push(50);
-
-
     cout<<"Display before adding ele. "<<endl;
+    
     stack<int>temp;
     while(st.size()>0){
         cout<<st.top()<<" ";
@@ -96,7 +93,6 @@ int main(){
     }
     cout<<endl;
     
-
     stack<int>temp2;
     while(st.size()>0){
         int x=st.top();
@@ -136,5 +132,116 @@ int main(){
 
 
 
+
+
+
+
+// By using function -: 
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void print(stack<int>st){
+    stack<int>temp;
+    while(st.size()>0){
+        cout<<st.top()<<" ";
+        temp.push(st.top());
+        st.pop();
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    print(st);
+
+    // push ele. at the bottom of the stack -: 
+    
+    stack<int>helper;
+    while(st.size()>0){
+        helper.push(st.top());
+        st.pop();
+    }
+
+//     now all the ele.s of the stack is comes in helper stacks, 
+//     now st stack is empty, you can insert ele. in the st stack 
+//     which goes to bottom of the st stack, and then again insert all the 
+//     ele.s of the helper stack to a st stack and then print 
+
+
+    st.push(1000);
+
+    while(helper.size()>0){
+        st.push(helper.top());
+        helper.pop();
+    }
+    print(st);
+
+}
+
+// 50 40 30 20 10 
+// 50 40 30 20 10 1000 
+
+
+
+// push ele. at the bottom of the stack by using function -:
+
+
+// Stack is pass by reference (&) to a function -: 
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void print(stack<int>st){
+    stack<int>temp;
+    while(st.size()>0){
+        cout<<st.top()<<" ";
+        temp.push(st.top());
+        st.pop();
+    }
+    cout<<endl;
+}
+
+// Stack is pass by reference (&) to a function -:
+void pushatbottom(stack<int>& st, int val){
+    stack<int>helper;
+    while(st.size()>0){
+        helper.push(st.top());
+        st.pop();
+    }
+
+    st.push(val);
+
+    while(helper.size()>0){
+        st.push(helper.top());
+        helper.pop();
+    }
+}
+
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    print(st);
+    pushatbottom(st,5000);
+    print(st);
+
+}
+
+// 50 40 30 20 10 
+// 50 40 30 20 10 5000 
 
 
