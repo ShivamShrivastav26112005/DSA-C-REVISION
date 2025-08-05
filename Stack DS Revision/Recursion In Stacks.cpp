40d66dbf63dbb5521bf0894ea5f29670bcb247ef
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-08-05 23:42:44 +0530

    Update Recursion In Stacks.cpp

--- Stack DS Revision/Recursion In Stacks.cpp
+++ Stack DS Revision/Recursion In Stacks.cpp
@@ -48,3 +48,99 @@ int main(){
 
 
 
+
+
+
+
+
+// Push At Bottom Recursively -: 
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+void printNor(stack<int>& st){
+    if(st.size()==0) return ;
+    int x=st.top();
+    cout<<x<<" ";
+    st.pop();
+    printNor(st);
+    st.push(x);
+}
+
+void printRev(stack<int>& st){
+    if(st.size()==0) return ;
+    int x=st.top();
+    st.pop();
+    printRev(st);
+    cout<<x<<" ";
+    st.push(x);
+}
+
+void pushatbottom(stack<int>& st, int val){
+    if(st.size()==0){ // base case 
+        st.push(val);
+        return;
+    }
+    int x=st.top(); // kaam
+    st.pop();
+    pushatbottom(st,val); // call 
+    st.push(x); 
+}
+
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    printNor(st);
+    cout<<endl;
+    printRev(st);
+    pushatbottom(st,1000);
+    cout<<endl;
+    printNor(st);
+    cout<<endl;
+    printRev(st);
+
+}
+
+
+// 50 40 30 20 10 
+// 10 20 30 40 50
+// 50 40 30 20 10 1000 
+// 1000 10 20 30 40 50
+
+
+
+
+
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    stack<int>st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    st.push(50);
+
+    stack<int>rt;
+    while(st.size()>0){
+        rt.push(st.top());
+        st.pop();
+    }
+
+    while(rt.size()>0){
+        cout<<rt.top()<<" ";
+        st.push(rt.top());
+        rt.pop();
+    }
+
+}
+// 10 20 30 40 50 
+
+// Done. 
