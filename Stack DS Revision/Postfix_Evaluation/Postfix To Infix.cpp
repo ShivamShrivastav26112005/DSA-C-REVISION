f7db87e26f787a5e1e1653250b815a5d34c6d3b1
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-08-16 18:46:18 +0530

    Postfix To Infix Using Single Stacks.

---
+++ Stack DS Revision/Postfix_Evaluation/Postfix To Infix.cpp
@@ -0,0 +1,56 @@
+// Q-> Postfix To Infix Conversion. 
+
+// We have to given postfix expression and we have to convert those to infix expression and return the ans using stack. 
+// Solved by using single stack. we make a stack "val" of type string.  
+// IMP -: prefix and postfix me brackets nhi hote hai infix me brackets hote hai 
+
+
+
+// -> Postfix To Infix Conversion -: 
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+// main hai hmara solve() fun -> hme infix(val1,oper,val2) banana hai and usse string me dalna hai 
+string solve(string val1, string val2, char ch){
+    string s="";
+    s+=val1; // value 1
+    s.push_back(ch); // operator 
+    s+=val2; // value 2
+    return s;
+}
+
+int main(){
+    string s="79+4*8/3-"; // given postfix expression 
+    // we need one stack -> values stack of string type 
+    stack<string>val;
+
+    // traverse in the string 
+    for(int i=0; i<s.length(); i++){
+        // check if s[i] is a digit (0-9) then push directly those no./value
+        if(s[i]>=48 && s[i]<=57){
+            val.push(to_string(s[i]-48)); // push values/no. as a string not a character so, we convert value to string using to_string() fun. 
+        }
+        else{ // else operator comes, s[i] -: it is -> *,/,+,-
+            // then do work / kaam 
+            string val2=val.top();
+            val.pop();
+            string val1=val.top();
+            val.pop();
+            string ans=solve(val1,val2,s[i]); 
+            val.push(ans);
+        }
+    }
+    cout<<val.top()<<endl; // now string is present at the top of the val stack 
+    cout<<"(7+9)*4/8-3"<<endl;
+    // Or -: 
+    cout<<"7+9*4/8-3";
+}
+
+// 7+9*4/8-3 -> we maked infix 
+// (7+9)*4/8-3 -> we have to given postfix
+// 7+9*4/8-3 -> it could also be given like this 
+
+
+
