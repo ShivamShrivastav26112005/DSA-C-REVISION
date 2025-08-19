1fb410056a798fdef54574f84c354bf0ad98fb0d
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-08-19 17:23:23 +0530

    Basic Implementation Queue.

--- Queue/Part1_Basic_Implementation_Queue.cpp
+++ Queue/Part1_Basic_Implementation_Queue.cpp
@@ -1,33 +1,38 @@
-// basic operations perform on the queues -: 
+// Basic operations perform on the queues -: 
 
-// #include<iostream>
-// #include<queue>
-// using namespace std;
-// int main(){
-//     queue<int>q;
-//     // operations -: push,pop,front/top,back,size,empty. 
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
-//     cout<<q.front()<<endl;
-//     cout<<q.back()<<endl;
-//     cout<<q.size()<<endl;
-//     q.pop(); // pop/remove/delete always front ele. of the queue 
-//     cout<<q.empty()<<endl;
-//     cout<<q.size()<<endl;
-//     cout<<q.front()<<endl;
-//     cout<<q.back()<<endl;
+#include<iostream>
+#include<queue>
+using namespace std;
 
-// }
-// // 10
-// // 50
-// // 5
-// // 0
-// // 4
-// // 20
-// // 50
+int main(){
+    queue<int>q;
+    // operations -: push,pop,front/top,back,size,empty. 
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+ 
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+    cout<<q.size()<<endl;
+ 
+    q.pop(); // pop/remove/delete always front ele. of the queue 
+ 
+    cout<<q.empty()<<endl;
+    cout<<q.size()<<endl;
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+
+}
+// 10
+// 50
+// 5
+// 0
+// 4
+// 20
+// 50
+
 
 
 
@@ -39,45 +44,47 @@
 
 
 
-// // Display the queue -: 
 
-// // (a)-: normal order or forward direction -> by using the same queue using for loop
-// // (b)-: reverse order or backward direction -> by using an extra stack using while loop 
+// Display the queue -: 
+
+// (a)-: normal order or forward direction -> by using the same queue using for loop
+// (b)-: reverse order or backward direction -> by using an extra stack using while loop 
 
 
-// #include<iostream>
-// #include<queue>
-// using namespace std;
+#include<iostream>
+#include<queue>
+using namespace std;
 
-// // queue is pass by reference (&) to a function. 
-// void display(queue<int>& q){
-//     int n=q.size();
-//     for(int i=0; i<n; i++){ // traverse in the given queue 
-//         int x=q.front();
-//         cout<<x<<" ";
-//         q.pop();
-//         q.push(x);
-//     }
-//     cout<<endl;
-// }
+// queue is pass by reference (&) to a function. 
+void display(queue<int>& q){
+    int n=q.size();
+    for(int i=0; i<n; i++){ // traverse in the given queue 
+        int x=q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
 
-// int main(){
-//     queue<int>q;
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
-//     // we make a function display where we pass our (q) to print/display the queue's ele.s 
-//     display(q);
-//     q.pop();
-//     display(q);
-// }
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    // we make a function display where we pass our (q) to print/display the queue's ele.s 
+    display(q);
+    q.pop();
+    display(q);
+}
 
-// // print from front to back in forward direction or normal order 
+// print from front to back in forward direction or normal order 
 
-// // 10 20 30 40 50 
-// // 20 30 40 50 
+// 10 20 30 40 50 
+// 20 30 40 50 
+
 
 
 
@@ -92,69 +99,68 @@
 // use (&) symbol while passing queue to a function 
 
 
-// // queue display by using for loop -:
-// // reverse queue by using an extra stack -: 
+// queue display by using for loop -:
+// reverse queue by using an extra stack -: 
 
-// #include<iostream>
-// #include<queue>
-// #include<stack>
-// using namespace std;
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
 
-// void display(queue<int>& q){
-//     int n=q.size();
-//     for(int i=0; i<n; i++){
-//         int x=q.front();
-//         cout<<x<<" ";
-//         q.pop();
-//         q.push(x);
-//     }
-//     cout<<endl;
-// }
+void display(queue<int>& q){
+    int n=q.size();
+    for(int i=0; i<n; i++){
+        int x=q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
 
-// void reverse(queue<int>& q){
-//     stack<int>st;
-//     // empty the queue into stack -: 
-//     while(q.size()>0){
-//         int x=q.front();
-//         q.pop();
-//         st.push(x);
-//     }
+void reverse(queue<int>& q){
+    stack<int>st;
+    // empty the queue into stack -: 
+    while(q.size()>0){
+        int x=q.front();
+        q.pop();
+        st.push(x);
+    }
 
-//     // empty the stack into queue -: 
-//     while(st.size()>0){
-//         int x=st.top();
-//         st.pop();
-//         cout<<x<<" ";
-//         q.push(x);
-//     }
+    // empty the stack into queue -: 
+    while(st.size()>0){
+        int x=st.top();
+        st.pop();
+        cout<<x<<" ";
+        q.push(x);
+    }
 
-// }
+}
 
-// int main(){
-//     queue<int>q;
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
-//     q.push(60);
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
 
-//     // cout<<q.front()<<endl;
-//     // cout<<q.back()<<endl;
-//     // cout<<q.size()<<endl;
-//     // q.pop();
-//     // cout<<q.front()<<endl;
-//     // cout<<q.back()<<endl;
-//     // cout<<q.size()<<endl;
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+    cout<<q.size()<<endl;
+    q.pop();
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+    cout<<q.size()<<endl;
 
+    display(q); // 10 20 30 40 50 60 
+    reverse(q);
 
-//     display(q); // 10 20 30 40 50 60 
-//     reverse(q);
+}
 
-// }
-
-// // 10 20 30 40 50 60 
-// // 60 50 40 30 20 10
+// 10 20 30 40 50 60 
+// 60 50 40 30 20 10
 
 
 
@@ -162,58 +168,61 @@
 
 
 
-// #include<iostream>
-// #include<queue>
-// #include<stack>
-// using namespace std;
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
 
-// void display(queue<int>& q){
-//     int n=q.size();
-//     for(int i=0; i<n; i++){
-//         int x=q.front();
-//         cout<<x<<" ";
-//         q.pop();
-//         q.push(x);
-//     }
-//     cout<<endl;
-// }
+void display(queue<int>& q){
+    int n=q.size();
+    for(int i=0; i<n; i++){
+        int x=q.front();
+        cout<<x<<" ";
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
 
-// void reverse(queue<int>& q){
-//     stack<int>st;
-//     // empty the queue into stack -: 
-//     while(q.size()>0){
-//         int x=q.front();
-//         q.pop();
-//         st.push(x);
-//     }
+void reverse(queue<int>& q){
+    stack<int>st;
+    // empty the queue into stack -: 
+    while(q.size()>0){
+        int x=q.front();
+        q.pop();
+        st.push(x);
+    }
 
-//     // empty the stack into queue -: 
-//     while(st.size()>0){
-//         int x=st.top();
-//         st.pop();
-//         q.push(x);
-//     }
-// }
+    // empty the stack into queue -: 
+    while(st.size()>0){
+        int x=st.top();
+        st.pop();
+        q.push(x);
+    }
+}
 
-// int main(){
-//     queue<int>q;
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
 
-//     display(q); 
-//     reverse(q);
-//     display(q); 
-//     reverse(q);
-//     display(q); 
+    display(q); 
+    reverse(q);
+    display(q); 
+    reverse(q);
+    display(q); 
 
-// }
+}
 
-// // 10 20 30 40 50 
-// // 50 40 30 20 10
-// // 10 20 30 40 50
+// 10 20 30 40 50 
+// 50 40 30 20 10
+// 10 20 30 40 50
+
+
+
 
 
 
@@ -223,37 +232,37 @@
 // Consider 0-based indexing. 
  
 
-// #include<iostream>
-// #include<queue>
-// #include<stack>
-// using namespace std;
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
 
-// void display(queue<int>& q){
-//     int n=q.size();
-//     for(int i=0; i<n; i++){
-//         int x=q.front();
-//         if(i%2!=0){ // display ele.s of the odd position
-//             cout<<x<<" ";
-//         }
-//         q.pop();
-//         q.push(x);
-//     }
-//     cout<<endl;
-// }
+void display(queue<int>& q){
+    int n=q.size();
+    for(int i=0; i<n; i++){
+        int x=q.front();
+        if(i%2!=0){ // display ele.s of the odd position
+            cout<<x<<" ";
+        }
+        q.pop();
+        q.push(x);
+    }
+    cout<<endl;
+}
 
-// int main(){
-//     queue<int>q;
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
 
-//     display(q);  
+    display(q);  
 
-// }
+}
 
-// // 20 40 
+// 20 40 
 
 
 
@@ -261,59 +270,59 @@
 
 
 
-// #include<iostream>
-// #include<queue>
-// using namespace std;
+#include<iostream>
+#include<queue>
+using namespace std;
 
-// void display(queue<int>& q){ // q is pass by reference to a fun. 
-//     int n=q.size();
-//     for(int i=0; i<n; i++){
-//         int x=q.front();
-//         q.pop();
-//         cout<<x<<" ";
-//         q.push(x);
-//     }
-//     cout<<endl;
-// }
+void display(queue<int>& q){ // q is pass by reference to a fun. 
+    int n=q.size();
+    for(int i=0; i<n; i++){
+        int x=q.front();
+        q.pop();
+        cout<<x<<" ";
+        q.push(x);
+    }
+    cout<<endl;
+}
 
-// void remove_even(queue<int>& q){
-//     int n=q.size();
-//     for(int i=0; i<n; i++){
+void remove_even(queue<int>& q){
+    int n=q.size();
+    for(int i=0; i<n; i++){
 
-//         // if(i%2==0) q.pop();
-//         // else{
-//         //     int x=q.front();
-//         //     q.pop();
-//         //     q.push(x);
-//         // }
+        if(i%2==0) q.pop();
+        else{
+            int x=q.front();
+            q.pop();
+            q.push(x);
+        }
 
-//         // OR -:
+        // OR -:
 
-//         if(i%2!=0){
-//             int x=q.front();
-//             q.push(x);
-//         }
-//         q.pop();
-//     }
+        if(i%2!=0){
+            int x=q.front();
+            q.push(x);
+        }
+        q.pop();
+    }
+}
 
-// }
-// int main(){
-//     queue<int>q;
-//     q.push(10);
-//     q.push(20);
-//     q.push(30);
-//     q.push(40);
-//     q.push(50);
-//     q.push(60);
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
 
-//     display(q);
-//     remove_even(q);
-//     display(q);
+    display(q);
+    remove_even(q);
+    display(q);
+}
 
-// }
+// 10 20 30 40 50 60 
+// 20 40 60 
 
-// // 10 20 30 40 50 60 
-// // 20 40 60 
 
 
 
