7b403d8f797ca6713c7a622862cef32e8fa2b27f
SHIVAM SHRIVASTAV <shivamshrivastav26112005@gmail.com>
2025-08-27 20:58:03 +0530

    More Problems Added On Special Algorithms.

--- Special_Algorithms/Prefix And Suffix Multiplication.cpp
+++ Special_Algorithms/Prefix And Suffix Multiplication.cpp
@@ -96,3 +96,84 @@ int main(){
 // After apply prefix sum : 21 20 18 15 11 6
 
 
+
+
+
+
+
+
+// Suffix Multiply  -: 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Before Apply prefix Sum : ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // Suffix multiply algo -:
+    int suff[n];
+    suff[n-1]=arr[n-1];
+    for(int i=n-2; i>=0; i--){
+        suff[i]=arr[i]*suff[i+1];
+    }
+    cout<<"After apply prefix sum : ";
+    for(int i=0; i<n; i++){
+        cout<<suff[i]<<" ";
+    }
+}
+
+// Before Apply prefix Sum : 1 2 3 4 5 6 
+// After apply prefix sum : 720 720 360 120 30 6 
+
+
+
+
+
+// Sliding window algo -:
+
+// find the maximum subarray sum from the given array with size k where array and the size k is given.
+
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    int arr[]={1,3,5,7,8};
+    int k=3;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    // sliding window algo used -: 
+    // first find previous sum then find current sum by apply sliding window algo -: 
+    int maxsum=INT_MIN;
+    int presum=0;
+    for(int i=0; i<k; i++){
+        presum+=arr[i];
+    }
+    maxsum=presum;
+    // sliding window alggo -: 
+    int i=1;
+    int j=k;
+    while(j<n){
+        int currsum=presum+arr[j]-arr[i-1];
+        if(currsum>maxsum){
+            maxsum=currsum;
+        }
+        presum=currsum;
+        i++;
+        j++;
+    }
+    cout<<maxsum;
+}
+
+// 1 3 5 7 8 
+// 20 -> this is the maximum sum.
+
+// Done -: 
